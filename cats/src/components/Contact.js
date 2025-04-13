7ee6320db3cd8c60d3175d2ea07910d9194b1627
Phuong <phuong.nguyen@alumni.fh-aachen.de>
2025-04-13 13:27:18 +0200

    Update Contact.js
    another way to use props

--- cats/src/components/Contact.js
+++ cats/src/components/Contact.js
@@ -2,7 +2,7 @@ import React from "react";
 
 import phoneIcon from "../images/phone-icon.jpg";
 import mailIcon from "../images/mail-icon.jpg";
-
+/*
 function Contact(props) {
     return(
         <div className="contact-card">
@@ -15,6 +15,23 @@ function Contact(props) {
             <div className="info-group">
                 <img src={mailIcon} alt="mail-icon" className="mail-icon" />
                 <p>{props.mail}</p>
+            </div>
+        </div>
+    )
+}*/
+function Contact({img, name, phone, mail}) {
+    // Destructuring props for better readability
+    return(
+        <div className="contact-card">
+            <img src={img} alt="Cat 1" className="contact--image" />
+            <h3>{name}</h3>
+            <div className="info-group">
+                <img src={phoneIcon} alt="phone-icon" className="phone--icon" />
+                <p>{phone}</p>
+            </div>
+            <div className="info-group">
+                <img src={mailIcon} alt="mail-icon" className="mail-icon" />
+                <p>{mail}</p>
             </div>
         </div>
     )
