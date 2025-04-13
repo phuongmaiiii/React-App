1fa222ba16fdee221f6adaf9d03041a5c4a6a977
Phuong <phuong.nguyen@alumni.fh-aachen.de>
2025-04-13 18:02:35 +0200

    using props

--- airbnb/src/App.js
+++ airbnb/src/App.js
@@ -2,13 +2,21 @@ import React from 'react';
 import Navbar from './components/Narbar';
 import Hero from './components/Hero';
 import Card from './components/Card';
+import logo from "./images/image1.png";
 
 function App() {
   return (
     <div className="App">
       <Navbar />
       <Hero />
-      <Card />
+      <Card 
+        img={logo}
+        rating="5.0"
+        reviewCount={6}
+        country="USA"
+        title="Life Lessons with Katie Zaferes"
+        price={136}
+      />
     </div>
   );
 }
--- airbnb/src/components/Card.js
+++ airbnb/src/components/Card.js
@@ -1,19 +1,19 @@
 import React from "react";
-import logo from "../images/image1.png";
+
 import star from "../images/Star.png";
 
-function Card() {
+function Card(props) {
     return(
         <div className="card">
-            <img src={logo} alt="Card Image 1" className="card--image" />
+            <img src={props.img} alt="Card Image 1" className="card--image" />
             <div className="card--info">
                 <img src={star} alt="Star" className="card--star" />
-                <span>5.0</span>
-                <span className="gray">(6) • </span>
-                <span className="gray">USA</span>
+                <span>{props.rating}</span>
+                <span className="gray">({props.reviewCount}) • </span>
+                <span className="gray">{props.country}</span>
             </div>
-            <p className="card--title">Life Lessons with Katie Zaferes</p>
-            <p className="card--price"><span className="bold">From $136</span> / person</p>
+            <p className="card--title">{props.title}</p>
+            <p className="card--price"><span className="bold">From ${props.price}</span> / person</p>
         </div>
     )
 }
