805e8cec99d311991194eee9f4bfcb48e2b8eb43
Phuong <phuong.nguyen@alumni.fh-aachen.de>
2025-07-18 18:30:00 +0200

    styling css

--- airbnb/src/data.js
+++ airbnb/src/data.js
@@ -1,4 +1,4 @@
-export default [
+const data = [
     {
         id: 1,
         title: "Life Lessons with Katie Zaferes",
@@ -38,7 +38,49 @@ export default [
         },
         location: "USA",
         openSpots: 3,
+    },
+       {
+        id: 4,
+        title: "Life Lessons with Katie Zaferes",
+        description: "Join Katie as she shares her life lessons and experiences with you. This is a great opportunity to learn from someone who has been through it all.",
+        price: 136,
+        coverImg: "image1.png",
+        stats: {
+            rating: 5.0,
+            reviewCount: 6
+        },
+        location: "USA",
+        openSpots: 0,
+
+    },
+    {
+        id: 5,
+        title: "Learn Wedding Photography",
+        description: "Interested in becoming a wedding photographer? For beginners and experienced photographers alike, this course will teach you everything you need to know about wedding photography.",
+        price: 125,
+        coverImg: "wedding-photography.png",
+        stats: {
+            rating: 5.0,
+            reviewCount: 30
+        },
+        location: "Online",
+        openSpots: 27,
+    },
+    {
+        id: 6,
+        title: "Group Mountain Biking",
+        description: "Join a group of mountain bikers for an unforgettable experience in the mountains. This is a great opportunity to meet new people and enjoy the outdoors.",
+        price: 50,
+        coverImg: "mountain-bike.png",
+        stats: {
+            rating: 4.8,
+            reviewCount: 2
+        },
+        location: "USA",
+        openSpots: 3,
     }
-   
+    
     
 ]
+
+export default data;
